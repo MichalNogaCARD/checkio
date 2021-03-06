afa2966404ef271b0329a5fa749b1c264761e7ad
Michal <michalnoga.poczta@gmail.com>
2021-03-06 20:11:54 +0100

    ELEMENTARY - 09 Replace First

---
+++ ELEMENTARY/09_REPLACE_FIRST/main.py
@@ -0,0 +1,19 @@
+from typing import Iterable
+from collections import deque
+
+
+def replace_first(items: list) -> Iterable:
+    deque_items = deque(items)
+    deque_items.rotate(-1)
+    return deque_items
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(list(replace_first([1, 2, 3, 4])))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(replace_first([1, 2, 3, 4])) == [2, 3, 4, 1]
+    assert list(replace_first([1])) == [1]
+    assert list(replace_first([])) == []
+    print("Coding complete? Click 'Check' to earn cool rewards!")
