534af3f403d41118218cd908bb57608ac38cfbd8
Michal <michalnoga.poczta@gmail.com>
2022-07-12 21:07:46 +0200

    ICE BASE - 05 How to find friends

---
+++ ICE_BASE/05_HOW_TO_FIND_FRIENDS/main.py
@@ -0,0 +1,31 @@
+from itertools import chain
+
+
+def check_connection(network, first, second) -> bool:
+    nodes = {tuple(connection.split('-')) for connection in network}
+    friends = {first}
+
+    while friends != set():
+        pairs = {pair for pair in nodes if any(one in pair for one in friends)}
+        friends = set(chain(*pairs)) - friends
+        if second in friends:
+            return True
+        nodes -= pairs
+
+    return False
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert check_connection(
+        ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
+         "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"),
+        "scout2", "scout3") == True, "Scout Brotherhood"
+    assert check_connection(
+        ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
+         "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"),
+        "super", "scout2") == True, "Super Scout"
+    assert check_connection(
+        ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
+         "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"),
+        "dr101", "sscout") == False, "I don't know any scouts."
